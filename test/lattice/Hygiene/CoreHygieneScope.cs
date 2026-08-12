fdbc7582c7a525e0bc8a7530413ffae70681d1a5
NSTA1 <NSTA1@users.noreply.github.com>
2026-08-12 07:34:07 +0100

    test: register lattice.storage.file hygiene slice roots (#1436)

--- test/lattice/Hygiene/CoreHygieneScope.cs
+++ test/lattice/Hygiene/CoreHygieneScope.cs
@@ -33,6 +33,7 @@ internal static class CoreHygieneScope
         "src/lattice.replication", "test/lattice.replication",
         "src/lattice.replication.grpc", "test/lattice.replication.grpc",
         "src/lattice.storage.azuretable", "test/lattice.storage.azuretable",
+        "src/lattice.storage.file", "test/lattice.storage.file",
         "src/lattice.backup.azureblob", "test/lattice.backup.azureblob",
         "src/lattice.caching.azureblob", "test/lattice.caching.azureblob",
         "src/lattice.scaling", "test/lattice.scaling",
