a9681299ebe1ec6b570a10415071dc9a933285e3
NSTA1 <NSTA1@users.noreply.github.com>
2026-09-25 18:40:20 +0100

    test(lattice): audit optimistic read state-machine reflection

--- test/lattice/BPlusTree/Grains/ShardRootGrainOptimisticReadTests.ReadCost.cs
+++ test/lattice/BPlusTree/Grains/ShardRootGrainOptimisticReadTests.ReadCost.cs
@@ -10,14 +10,18 @@ public sealed partial class ShardRootGrainOptimisticReadTests
     [Test]
     public void Raw_read_state_machine_does_not_capture_leaf_proof()
     {
-        var stateMachine = typeof(ShardRootGrain).GetMethod(nameof(ShardRootGrain.TryGetOptimisticAsync))!
-            .GetCustomAttribute<AsyncStateMachineAttribute>()!.StateMachineType;
+        var readMethod = typeof(ShardRootGrain).GetMethod(nameof(ShardRootGrain.TryGetOptimisticAsync));
+        Assert.That(readMethod, Is.Not.Null, "Optimistic read entry point changed; update this structural guard.");
+        var stateMachineAttribute = readMethod!.GetCustomAttribute<AsyncStateMachineAttribute>();
+        Assert.That(stateMachineAttribute, Is.Not.Null, "Optimistic read no longer has an async state machine; update this guard.");
+        var stateMachine = stateMachineAttribute!.StateMachineType;
         var fields = stateMachine.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
         Assert.That(fields.Select(f => f.FieldType), Does.Not.Contain(typeof((Guid, long))));
         Assert.That(fields.Select(f => f.FieldType), Does.Not.Contain(typeof(TaskAwaiter<VersionedValue>)));
-        var size = typeof(ShardRootGrainOptimisticReadTests)
-            .GetMethod(nameof(ReadStateMachineSize), BindingFlags.Static | BindingFlags.NonPublic)!
-            .MakeGenericMethod(stateMachine).Invoke(null, null);
+        var sizeMethod = typeof(ShardRootGrainOptimisticReadTests)
+            .GetMethod(nameof(ReadStateMachineSize), BindingFlags.Static | BindingFlags.NonPublic);
+        Assert.That(sizeMethod, Is.Not.Null, "Test-only size helper changed; update this guard.");
+        var size = sizeMethod!.MakeGenericMethod(stateMachine).Invoke(null, null);
         TestContext.Out.WriteLine($"COST raw-read-state-machine bytes={size}");
     }
 
